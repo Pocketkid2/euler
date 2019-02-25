b6ec7ab53f92f47633542f6c4e1757a73f1bfce0
Adam <afranklint16@gmail.com>
2019-02-24 19:22:03 -0800

    Commit first file for 36

---
+++ 36/main.c
@@ -0,0 +1,18 @@
+// ----------------------------------------
+//	PROJECT EULER PROBLEM 36
+//		https://projecteuler.net/problem=36
+// ----------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <inttypes.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+}
