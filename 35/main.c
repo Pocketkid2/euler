25ffd0ca9df6be302e10f37bff958bc78750edbd
Adam <afranklint16@gmail.com>
2019-02-24 20:07:14 -0800

    [35] Finish implementation

--- 35/main.c
+++ 35/main.c
@@ -15,6 +15,13 @@
 
 #define SIZE 1000000
 #define SQRT 1000   // <- Pre-calculated square root of the size (for simplicity and efficiency)
+#define STRLEN 10
+
+// Checks if the prime given is a circular number in base 10
+bool circular(int i, bool prime[SIZE]);
+
+// Rotates the given string in-place by 1
+void rotate(char *str);
 
 int main(int argc, char *argv[]) {
 
@@ -33,11 +40,45 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Test by printing all primes
+    int count = 0;
+
+    // Print all circular primes
     for (int i = 2; i < SIZE; i++) {
         if (prime[i]) {
-            printf("%d\n", i);
+            if (circular(i, prime)) {
+                printf("Found circular prime: %d\n", i);
+                count++;
+            }
         }
     }
 
+    printf("Number of circular primes below %d: %d\n", SIZE, count);
+
+}
+
+// Assumes that i is already known to be a prime
+bool circular(int i, bool prime[SIZE]) {
+    char string[STRLEN];
+    char modify[STRLEN];
+    itoa(i, string, 10);
+    strcpy(modify, string);
+    rotate(modify);
+    while (strcmp(string, modify)) {
+        if (!prime[atoi(modify)]) {
+            return false;
+        }
+        rotate(modify);
+
+    }
+    return true;
+}
+
+// Rotate the given string by 1 to the left (i.e. grab first char and move to end)
+void rotate(char *str) {
+    char first = *str;
+    int len = strlen(str);
+    for (int i = 0; i < (len - 1); i++) {
+        str[i] = str[i+1];
+    }
+    str[len - 1] = first;
 }
